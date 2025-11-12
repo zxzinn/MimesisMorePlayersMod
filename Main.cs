e27fe90c5d3f01c626ec276badaaa2514992cf74
zxzinn <92992703+zxzinn@users.noreply.github.com>
2025-11-13 05:12:45 +0800

    Fix infinite loading issue - Remove PATCH 5 (GetMemberCount)
    - Removed PATCH 5 that returned 0 for GetMemberCount
    - This patch was breaking SessionManager.GetSessionCount calculation
    - Caused infinite loading when entering tram lobby (waiting room)
    - Reduced total patches from 12 to 11
    - Bumped version to 1.4.2
    
    Technical details:
    GetSessionCount uses GetRoomMemberCount to calculate player counts.
    When GetMemberCount returned 0, it miscalculated expected player count,
    causing the game to wait forever for non-existent players to load.

---
+++ Main.cs
@@ -0,0 +1,440 @@
+using MelonLoader;
+using HarmonyLib;
+using System;
+using System.Linq;
+using System.Reflection;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+[assembly: MelonInfo(typeof(MorePlayers.MorePlayersMod), "MorePlayers", "1.4.2", "github.com/zxzinn")]
+[assembly: MelonGame("ReLUGames", "MIMESIS")]
+
+namespace MorePlayers
+{
+    public class MorePlayersMod : MelonMod
+    {
+        public const int MAX_PLAYERS = 999;
+
+        public override void OnInitializeMelon()
+        {
+            MelonLogger.Msg("=================================================");
+            MelonLogger.Msg("MorePlayers Mod v1.4.2");
+            MelonLogger.Msg("=================================================");
+            MelonLogger.Msg("Author: github.com/zxzinn");
+            MelonLogger.Msg($"Max Players: {MAX_PLAYERS}");
+            MelonLogger.Msg("");
+
+            var harmony = new HarmonyLib.Harmony("com.moreplayers.mod");
+            harmony.PatchAll(typeof(MorePlayersMod).Assembly);
+
+            MelonLogger.Msg("");
+            MelonLogger.Msg("All patches applied!");
+            MelonLogger.Msg("=================================================");
+        }
+    }
+
+    // PATCH 1: ServerSocket.GetMaximumClients
+    [HarmonyPatch]
+    public class GetMaximumClients_Patch
+    {
+        static MethodBase TargetMethod()
+        {
+            try
+            {
+                var assembly = AppDomain.CurrentDomain.GetAssemblies()
+                    .FirstOrDefault(a => a.GetName().Name == "Assembly-CSharp");
+                var serverSocketType = assembly?.GetType("FishySteamworks.Server.ServerSocket");
+                var method = serverSocketType?.GetMethod("GetMaximumClients",
+                    BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public);
+
+                if (method != null)
+                    MelonLogger.Msg("[✓] ServerSocket.GetMaximumClients");
+
+                return method;
+            }
+            catch { return null; }
+        }
+
+        static bool Prefix(ref int __result)
+        {
+            __result = MorePlayersMod.MAX_PLAYERS;
+            return false;
+        }
+    }
+
+    // PATCH 2: ServerSocket.SetMaximumClients
+    [HarmonyPatch]
+    public class SetMaximumClients_Patch
+    {
+        static MethodBase TargetMethod()
+        {
+            try
+            {
+                var assembly = AppDomain.CurrentDomain.GetAssemblies()
+                    .FirstOrDefault(a => a.GetName().Name == "Assembly-CSharp");
+                var serverSocketType = assembly?.GetType("FishySteamworks.Server.ServerSocket");
+                var method = serverSocketType?.GetMethod("SetMaximumClients",
+                    BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public);
+
+                if (method != null)
+                    MelonLogger.Msg("[✓] ServerSocket.SetMaximumClients");
+
+                return method;
+            }
+            catch { return null; }
+        }
+
+        static bool Prefix(ref int value)
+        {
+            if (value < MorePlayersMod.MAX_PLAYERS)
+            {
+                value = MorePlayersMod.MAX_PLAYERS;
+            }
+            return true;
+        }
+    }
+
+    // PATCH 3: ServerSocket Constructor
+    [HarmonyPatch]
+    public class ServerSocket_Constructor_Patch
+    {
+        static MethodBase TargetMethod()
+        {
+            try
+            {
+                var assembly = AppDomain.CurrentDomain.GetAssemblies()
+                    .FirstOrDefault(a => a.GetName().Name == "Assembly-CSharp");
+                var serverSocketType = assembly?.GetType("FishySteamworks.Server.ServerSocket");
+                var ctor = serverSocketType?.GetConstructors(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic)
+                    .FirstOrDefault();
+
+                if (ctor != null)
+                    MelonLogger.Msg("[✓] ServerSocket Constructor");
+
+                return ctor;
+            }
+            catch { return null; }
+        }
+
+        static void Postfix(object __instance)
+        {
+            try
+            {
+                var type = __instance.GetType();
+                var setMethod = type.GetMethod("SetMaximumClients",
+                    BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public);
+                setMethod?.Invoke(__instance, new object[] { MorePlayersMod.MAX_PLAYERS });
+            }
+            catch { }
+        }
+    }
+
+    // PATCH 4: IVroom.CanEnterChannel
+    [HarmonyPatch]
+    public class IVroom_CanEnterChannel_Patch
+    {
+        static MethodBase TargetMethod()
+        {
+            try
+            {
+                var assembly = AppDomain.CurrentDomain.GetAssemblies()
+                    .FirstOrDefault(a => a.GetName().Name == "Assembly-CSharp");
+                var ivroomType = assembly?.GetType("IVroom");
+                var method = ivroomType?.GetMethod("CanEnterChannel",
+                    BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public);
+
+                if (method != null)
+                    MelonLogger.Msg("[✓] IVroom.CanEnterChannel");
+
+                return method;
+            }
+            catch { return null; }
+        }
+
+        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            var codes = new List<CodeInstruction>(instructions);
+
+            for (int i = 0; i < codes.Count; i++)
+            {
+                if (codes[i].opcode == OpCodes.Ldc_I4_4)
+                {
+                    codes[i] = new CodeInstruction(OpCodes.Ldc_I4, MorePlayersMod.MAX_PLAYERS);
+                }
+            }
+
+            return codes;
+        }
+    }
+
+    // PATCH 5: GameSessionInfo.AddPlayerSteamID
+    [HarmonyPatch]
+    public class GameSessionInfo_AddPlayerSteamID_Patch
+    {
+        static MethodBase TargetMethod()
+        {
+            try
+            {
+                var assembly = AppDomain.CurrentDomain.GetAssemblies()
+                    .FirstOrDefault(a => a.GetName().Name == "Assembly-CSharp");
+                var type = assembly?.GetType("GameSessionInfo");
+                var method = type?.GetMethod("AddPlayerSteamID",
+                    BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public);
+
+                if (method != null)
+                    MelonLogger.Msg("[✓] GameSessionInfo.AddPlayerSteamID");
+
+                return method;
+            }
+            catch { return null; }
+        }
+
+        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            var codes = new List<CodeInstruction>(instructions);
+
+            for (int i = 0; i < codes.Count; i++)
+            {
+                if (codes[i].opcode == OpCodes.Ldc_I4_4)
+                {
+                    codes[i] = new CodeInstruction(OpCodes.Ldc_I4, MorePlayersMod.MAX_PLAYERS);
+                }
+            }
+
+            return codes;
+        }
+    }
+
+    // PATCH 6: VRoomManager.EnterMaintenenceRoom
+    [HarmonyPatch]
+    public class VRoomManager_EnterMaintenenceRoom_Patch
+    {
+        static MethodBase TargetMethod()
+        {
+            try
+            {
+                var assembly = AppDomain.CurrentDomain.GetAssemblies()
+                    .FirstOrDefault(a => a.GetName().Name == "Assembly-CSharp");
+                var vroomManagerType = assembly?.GetType("VRoomManager");
+                var method = vroomManagerType?.GetMethod("EnterMaintenenceRoom",
+                    BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public);
+
+                if (method != null)
+                    MelonLogger.Msg("[✓] VRoomManager.EnterMaintenenceRoom");
+
+                return method;
+            }
+            catch { return null; }
+        }
+
+        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            var codes = new List<CodeInstruction>(instructions);
+
+            for (int i = 0; i < codes.Count; i++)
+            {
+                if (codes[i].opcode == OpCodes.Ldc_I4_4)
+                {
+                    codes[i] = new CodeInstruction(OpCodes.Ldc_I4, MorePlayersMod.MAX_PLAYERS);
+                }
+            }
+
+            return codes;
+        }
+    }
+
+    // PATCH 7: VRoomManager.EnterWaitingRoom
+    [HarmonyPatch]
+    public class VRoomManager_EnterWaitingRoom_Patch
+    {
+        static MethodBase TargetMethod()
+        {
+            try
+            {
+                var assembly = AppDomain.CurrentDomain.GetAssemblies()
+                    .FirstOrDefault(a => a.GetName().Name == "Assembly-CSharp");
+                var vroomManagerType = assembly?.GetType("VRoomManager");
+                var method = vroomManagerType?.GetMethod("EnterWaitingRoom",
+                    BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public);
+
+                if (method != null)
+                    MelonLogger.Msg("[✓] VRoomManager.EnterWaitingRoom");
+
+                return method;
+            }
+            catch { return null; }
+        }
+
+        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            var codes = new List<CodeInstruction>(instructions);
+
+            for (int i = 0; i < codes.Count; i++)
+            {
+                if (codes[i].opcode == OpCodes.Ldc_I4_4)
+                {
+                    codes[i] = new CodeInstruction(OpCodes.Ldc_I4, MorePlayersMod.MAX_PLAYERS);
+                }
+            }
+
+            return codes;
+        }
+    }
+
+    // PATCH 8: VRoomManager.PendStartGame
+    [HarmonyPatch]
+    public class VRoomManager_PendStartGame_Patch
+    {
+        static MethodBase TargetMethod()
+        {
+            try
+            {
+                var assembly = AppDomain.CurrentDomain.GetAssemblies()
+                    .FirstOrDefault(a => a.GetName().Name == "Assembly-CSharp");
+                var vroomManagerType = assembly?.GetType("VRoomManager");
+                var method = vroomManagerType?.GetMethod("PendStartGame",
+                    BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public);
+
+                if (method != null)
+                    MelonLogger.Msg("[✓] VRoomManager.PendStartGame");
+
+                return method;
+            }
+            catch { return null; }
+        }
+
+        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            var codes = new List<CodeInstruction>(instructions);
+
+            for (int i = 0; i < codes.Count; i++)
+            {
+                if (codes[i].opcode == OpCodes.Ldc_I4_3)
+                {
+                    codes[i] = new CodeInstruction(OpCodes.Ldc_I4, MorePlayersMod.MAX_PLAYERS);
+                }
+            }
+
+            return codes;
+        }
+    }
+
+    // PATCH 9: VRoomManager.PendStartSession
+    [HarmonyPatch]
+    public class VRoomManager_PendStartSession_Patch
+    {
+        static MethodBase TargetMethod()
+        {
+            try
+            {
+                var assembly = AppDomain.CurrentDomain.GetAssemblies()
+                    .FirstOrDefault(a => a.GetName().Name == "Assembly-CSharp");
+                var vroomManagerType = assembly?.GetType("VRoomManager");
+                var method = vroomManagerType?.GetMethod("PendStartSession",
+                    BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public);
+
+                if (method != null)
+                    MelonLogger.Msg("[✓] VRoomManager.PendStartSession");
+
+                return method;
+            }
+            catch { return null; }
+        }
+
+        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            var codes = new List<CodeInstruction>(instructions);
+
+            for (int i = 0; i < codes.Count; i++)
+            {
+                if (codes[i].opcode == OpCodes.Ldc_I4_3)
+                {
+                    codes[i] = new CodeInstruction(OpCodes.Ldc_I4, MorePlayersMod.MAX_PLAYERS);
+                }
+            }
+
+            return codes;
+        }
+    }
+
+    // PATCH 10: VRoomManager.OnFinishGame
+    [HarmonyPatch]
+    public class VRoomManager_OnFinishGame_Patch
+    {
+        static MethodBase TargetMethod()
+        {
+            try
+            {
+                var assembly = AppDomain.CurrentDomain.GetAssemblies()
+                    .FirstOrDefault(a => a.GetName().Name == "Assembly-CSharp");
+                var vroomManagerType = assembly?.GetType("VRoomManager");
+                var method = vroomManagerType?.GetMethod("OnFinishGame",
+                    BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public);
+
+                if (method != null)
+                    MelonLogger.Msg("[✓] VRoomManager.OnFinishGame");
+
+                return method;
+            }
+            catch { return null; }
+        }
+
+        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            var codes = new List<CodeInstruction>(instructions);
+
+            for (int i = 0; i < codes.Count; i++)
+            {
+                if (codes[i].opcode == OpCodes.Ldc_I4_3)
+                {
+                    codes[i] = new CodeInstruction(OpCodes.Ldc_I4, MorePlayersMod.MAX_PLAYERS);
+                }
+            }
+
+            return codes;
+        }
+    }
+
+    // PATCH 11: CreateLobby
+    [HarmonyPatch(typeof(SteamInviteDispatcher), "CreateLobby")]
+    public class SteamLobbyCreation_Patch
+    {
+        static bool Prefix(bool isOpenForRandomMatch)
+        {
+            try
+            {
+                var steamMatchmakingType = Type.GetType("Steamworks.SteamMatchmaking, com.rlabrecque.steamworks.net");
+                var eLobbyTypeType = Type.GetType("Steamworks.ELobbyType, com.rlabrecque.steamworks.net");
+                var playerPrefsType = Type.GetType("UnityEngine.PlayerPrefs, UnityEngine.CoreModule");
+
+                if (steamMatchmakingType == null || eLobbyTypeType == null || playerPrefsType == null)
+                {
+                    return true;
+                }
+
+                var createLobbyMethod = steamMatchmakingType.GetMethod("CreateLobby",
+                    BindingFlags.Public | BindingFlags.Static);
+                var setIntMethod = playerPrefsType.GetMethod("SetInt",
+                    BindingFlags.Public | BindingFlags.Static);
+
+                if (createLobbyMethod == null || setIntMethod == null)
+                {
+                    return true;
+                }
+
+                MelonLogger.Msg($"[✓] CreateLobby: {MorePlayersMod.MAX_PLAYERS} slots");
+
+                var friendsOnly = Enum.ToObject(eLobbyTypeType, 2);
+                createLobbyMethod.Invoke(null, new object[] { friendsOnly, MorePlayersMod.MAX_PLAYERS });
+                setIntMethod.Invoke(null, new object[] { "TempLobbyIsOpen", isOpenForRandomMatch ? 1 : 0 });
+
+                return false;
+            }
+            catch (Exception ex)
+            {
+                MelonLogger.Error($"CreateLobby patch error: {ex.Message}");
+                return true;
+            }
+        }
+    }
+}
---
+++ thunderstore/README.md
@@ -0,0 +1,382 @@
+# MorePlayers Mod for MIMESIS
+
+Remove the 4-player limit in MIMESIS multiplayer sessions.
+
+![Version](https://img.shields.io/badge/version-1.0.5-blue)
+![Game](https://img.shields.io/badge/game-MIMESIS-purple)
+![MelonLoader](https://img.shields.io/badge/MelonLoader-0.6.1+-green)
+![Status](https://img.shields.io/badge/status-working-brightgreen)
+
+## 📖 Description
+
+This mod patches the multiplayer player limit in MIMESIS, allowing more than 4 players to join a single session. The mod uses HarmonyX patches to modify server-side validation checks.
+
+**Default limit:** 4 players  
+**Modified limit:** 999 players (effectively unlimited)
+
+### How It Works
+
+The mod patches multiple validation points:
+1. **Network Layer:** `FishySteamworks.Server.ServerSocket` - Steam networking limits
+2. **Room Validation:** `VRoomManager.EnterWaitingRoom` - Server-side room entry checks  
+3. **Member Count:** `VWaitingRoom.GetMemberCount()` - Player count validation
+
+> ⚠️ **Important:** While the mod removes the technical limit, the actual number of players depends on:
+> - Host's network bandwidth and latency
+> - Steam P2P connection capabilities
+> - Game performance (more players = more resource usage)
+
+## 🎯 Who Needs This Mod?
+
+### ✅ **ONLY THE HOST** needs to install this mod!
+
+The mod patches **server-side validation** that happens on the host's game instance. Players joining the lobby **do NOT need** to install the mod.
+
+**Installation:**
+- **Host (lobby creator):** ✅ Must install mod
+- **Joining players:** ❌ No mod needed
+
+This makes it easy to play with friends - only the person hosting needs the mod!
+
+---
+
+## 🚀 Quick Start
+
+```
+1. Download MorePlayers.dll
+2. Place in: <MIMESIS>/Mods/MorePlayers.dll
+3. HOST creates lobby (mod installed)
+4. Friends join (NO mod needed)
+5. Enjoy 5+ player sessions! 🎉
+```
+
+**📌 Remember:** Only the HOST (lobby creator) needs the mod installed!
+
+---
+
+## ✨ Features
+
+- ✅ Removes 4-player limit
+- ✅ Patches server-side player count validation
+- ✅ Logging for debugging
+- ✅ No game file modifications required
+- ✅ Easy to install and uninstall
+
+## 📋 Requirements
+
+- **MIMESIS** (Steam version)
+- **[MelonLoader](https://github.com/LavaGang/MelonLoader/releases)** v0.6.1 or higher
+- Windows OS
+- .NET Framework 4.7.2 or higher
+
+## 🔧 Installation
+
+### Step 1: Install MelonLoader
+
+1. Download the latest MelonLoader installer from [GitHub Releases](https://github.com/LavaGang/MelonLoader/releases)
+2. Run the installer and select your MIMESIS installation folder:
+   - Default Steam location: `C:\Program Files (x86)\Steam\steamapps\common\MIMESIS`
+   - Or right-click MIMESIS in Steam → Manage → Browse local files
+3. Click Install
+4. Launch the game once to let MelonLoader initialize (game will close automatically)
+
+### Step 2: Install the Mod
+
+1. Download `MorePlayers.dll` from [Releases](../../releases)
+2. Copy `MorePlayers.dll` to your MIMESIS Mods folder:
+   ```
+   <MIMESIS_Install_Folder>/Mods/MorePlayers.dll
+   ```
+3. Launch the game
+
+### Verify Installation
+
+Check if the mod loaded successfully:
+1. Navigate to `<MIMESIS_Install_Folder>/MelonLoader/Latest.log`
+2. Look for these lines:
+   ```
+   [MorePlayers] MorePlayers Mod Loaded!
+   [MorePlayers] Applying Harmony patches...
+   [MorePlayers] Harmony patches applied successfully!
+   ```
+
+## 🎮 Usage
+
+Once installed, the mod works automatically:
+
+1. **Host a game** - The player limit is now 999
+2. **Check the log** - When creating a lobby, you'll see:
+   ```
+   [MorePlayers] SetMaximumClients(4) called, setting to 999 instead
+   [MorePlayers] GetMaximumClients() called, returning 999
+   ```
+3. **Invite players** - You can now have more than 4 players in your session!
+
+## 🔍 How It Works
+
+The mod uses [HarmonyX](https://github.com/BepInEx/HarmonyX) to patch multiple server-side methods:
+
+### Active Patches (6 total)
+
+1. **GetMaximumClients()** - Prefix patch returns 999
+2. **SetMaximumClients()** - Prefix patch prevents setting limit < 999
+3. **ServerSocket Constructor** - Postfix sets `_maximumClients = 999`
+4. **ServerSocket Methods** - IL Transpiler replaces field reads
+5. **EnterWaitingRoom()** - IL Transpiler (attempts to replace constant 4)
+6. **GetMemberCount()** - Prefix patch returns 0 to bypass `>= 4` check ⭐ **KEY PATCH**
+
+### Key Innovation - PATCH 6
+
+Instead of trying to modify the check `if (count >= 4)`, we make `GetMemberCount()` return `0`:
+```csharp
+// Original code:
+if (vwaitingRoom.GetMemberCount() >= 4) { /* block player */ }
+
+// With our patch:
+if (0 >= 4) { /* never executes! */ }
+```
+
+**Target Classes:** 
+- `FishySteamworks.Server.ServerSocket`
+- `VRoomManager`  
+- `VWaitingRoom`
+
+## 🎮 Testing the Mod
+
+### Expected Behavior
+
+When the 5th player tries to join your lobby:
+
+1. **In the log** you should see:
+   ```
+   [PATCH 6] GetMemberCount() called - actual: 4, returning: 0 (to bypass >= 4 check)
+   ```
+
+2. **Player successfully joins** instead of getting "Lobby Full" error
+
+3. **You can repeat** for 6th, 7th, 8th+ players
+
+### How to Test
+
+1. **Host creates lobby** (host must have mod installed)
+2. **4 players join** (no mod needed for them)
+3. **5th player attempts to join** (watch the log!)
+4. **Check results:**
+   - ✅ Success: Player joins, log shows PATCH 6 messages
+   - ❌ Failed: Player blocked, send me the full log
+
+### Verifying Installation
+
+Check `MelonLoader/Latest.log` for:
+
+```
+MorePlayers Mod v1.0.3 - Initializing...
+SUCCESS: All Harmony patches applied!
+Active patches:
+  [1] GetMaximumClients() - Prefix
+  [2] SetMaximumClients() - Prefix
+  [3] Constructor - Postfix
+  [4] Transpiler - IL Code Modification
+  [5] EnterWaitingRoom - Transpiler (VRoomManager)
+  [6] DISABLED (was causing crashes)
+
+[PATCH 6] Target found: VWaitingRoom.GetMemberCount()
+[PATCH 6] Will return max(actualCount, 0) to bypass >= 4 check
+```
+
+If you see this, the mod is loaded correctly! ✅
+
+## 🐛 Troubleshooting
+
+### Mod doesn't load (0 Mods loaded)
+
+**Check:**
+```powershell
+# Verify the file exists
+Test-Path "<MIMESIS_Folder>/Mods/MorePlayers.dll"
+```
+
+**Solutions:**
+- Ensure MelonLoader is properly installed
+- Unblock the DLL: Right-click → Properties → Check "Unblock" → Apply
+- Make sure the file is in the correct `Mods` folder
+- Restart the game
+
+### Harmony patch errors in log
+
+If you see errors like:
+```
+HarmonyLib.HarmonyException: Patching exception in method...
+```
+
+**Possible causes:**
+- Game was updated and code structure changed
+- Conflict with another mod
+- Corrupted mod file
+
+**Solutions:**
+- Download the latest version of the mod
+- Try disabling other mods temporarily
+- Check the [Issues](../../issues) page
+
+### Game crashes on startup
+
+1. Remove the mod temporarily:
+   ```powershell
+   del "<MIMESIS_Folder>/Mods/MorePlayers.dll"
+   ```
+2. Check the last lines in `MelonLoader/Latest.log` before the crash
+3. Report the issue with the log file
+
+### Players still can't join after 4
+
+**Possible reasons:**
+- Steam P2P connection limits
+- Host's network configuration (NAT, firewall)
+- Additional client-side checks (not yet patched)
+- Game server browser limitations
+
+**Check the log** for messages like:
+```
+[MorePlayers] GetMaximumClients() called, returning 999
+```
+If you see this, the mod is working, but there might be other limitations.
+
+## 🏗️ Building from Source
+
+### Prerequisites
+- Visual Studio 2019+ or MSBuild
+- .NET Framework 4.7.2 SDK
+
+### Build Steps
+
+1. Clone the repository:
+   ```bash
+   git clone https://github.com/yourusername/mimesis-moreplayers.git
+   cd mimesis-moreplayers
+   ```
+
+2. Copy game assemblies to `Libs/` folder:
+   ```
+   Libs/
+   ├── Assembly-CSharp.dll      (from MIMESIS_Data/Managed)
+   ├── UnityEngine.dll
+   ├── UnityEngine.CoreModule.dll
+   ├── netstandard.dll
+   ├── MelonLoader.dll          (from MelonLoader/net35)
+   └── 0Harmony.dll
+   ```
+
+3. Build the project:
+   ```powershell
++  MSBuild.exe MorePlayers.csproj /p:Configuration=Release  
+   ```
+
+4. Output will be in `Output/MorePlayers.dll`
+
+## 📝 Changelog
+
+### Version 1.0.5 (Current) - UI Crash Fix! 🛡️
+
+**CRITICAL FIX:**
+- **[PATCH 11]** UI Bounds Checking - Prevents crashes with 5+ players
+  - **Problem:** UI has only 4 slots → crashes when 5+ players join
+  - **Solution:** Cyclic slot usage - 5th player uses slot 0, 6th uses slot 1, etc.
+  - **Impact:** No more crashes! Game stable with unlimited players
+- **Code Cleanup:** Removed all commented-out patches, clean code structure
+- **All Patches:** 11 total (9 core + 3 UI protection)
+
+### Version 1.0.5 - Results Screen Fix! 🎯
+
+**CRITICAL FIX:**
+- **[PATCH 10]** ⭐ DeathMatchPlayerResult Array Expansion
+  - **Problem:** Results screen doesn't show after match with 5+ players
+  - **Cause:** Arrays hardcoded to size 4: `new DeathMatchPlayerResult[4]`
+  - **Solution:** Expands all result arrays from 4 to 999 slots
+  - **Impact:** Results screen now works with unlimited players!
+
+**How it works:**
+- Scans all methods working with `DeathMatchPlayerResult[]`
+- Finds IL code creating arrays: `ldc.i4.4; newarr DeathMatchPlayerResult`
+- Replaces size 4 with 999 using IL Transpiler
+- Covers: ResultScreen, ScoreBoard, DeathMatch, and all ReluProtocol classes
+
+**All Patches (10 total):** Network (1-4), Rooms (5, 8), Validation (6, 7), Steam (9), Results (10)
+
+### Version 1.0.4 - BREAKTHROUGH! 🚀
+
+**CRITICAL FIXES based on working mod:**
+- **[PATCH 7]** ⭐⭐ `CanEnterChannel()` - THE PRIMARY validation method!
+  - This is the REAL check that decides if players can join
+  - Patches both VWaitingRoom and MaintenanceRoom
+- **[PATCH 5 & 8]** ⭐ Set `_maxPlayers = 999` in rooms
+  - We were missing this critical field!
+  - VWaitingRoom and MaintenanceRoom now have correct limit
+- **[PATCH 9]** ⭐ Steam Lobby Creation
+  - Replaces hardcoded `4` with `999` in `SteamInviteDispatcher.CreateLobby()`
+  - Steam lobby now created with 999 slots
+
+**Why This Version Will Work:**
+- Found and adapted code from a **WORKING BepInEx mod**
+- Patches the ACTUAL validation method (`CanEnterChannel`)
+- Sets the ACTUAL limit field (`_maxPlayers`)
+- Patches the ACTUAL Steam lobby creation
+
+**All Patches (9 total):** Network layer (1-4), Room setup (5, 8), Validation (6, 7), Steam (9)
+
+### Version 1.0.3
+- **CRITICAL FIX:** Added patch for `VWaitingRoom.GetMemberCount()`
+- This was the main blocker preventing 5+ players from joining
+- Improved patch strategy: instead of modifying constants, intercepts the count check
+- Enhanced logging in English for easier debugging
+- Disabled aggressive global scanner that caused crashes
+- **All patches:** 6 total (5 active + 1 safety disabled)
+
+### Version 1.0.2
+- Added patch for `VRoomManager.EnterWaitingRoom`
+- Enhanced logging system
+
+### Version 1.0.1
+- Improved logging (English)
+- Added transpiler patches
+
+### Version 1.0.0
+- Initial release
+
+## 🤝 Contributing
+
+Contributions are welcome! Please:
+1. Fork the repository
+2. Create a feature branch
+3. Make your changes
+4. Test thoroughly
+5. Submit a pull request
+
+## ⚠️ Disclaimer
+
+- This mod is not affiliated with or endorsed by the developers of MIMESIS
+- Use at your own risk
+- Online multiplayer modifications may violate terms of service
+- The mod author is not responsible for any issues, bans, or data loss
+- Always backup your save files before using mods
+
+## 📄 License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE.md) file for details.
+
+## 🙏 Credits
+
+- **Harmony** - [Harmony Patching Library](https://github.com/pardeike/Harmony)
+- **MelonLoader** - [MelonLoader Mod Loader](https://github.com/LavaGang/MelonLoader)
+- **MIMESIS** - Game by ReLUGames
+- **FishySteamworks** - Steam integration for FishNet
+
+## 📞 Support
+
+- 🐛 [Report Issues](../../issues)
+- 💬 [Discussions](../../discussions)
+- 📧 Contact: andy@0c.md
+
+---
+
+**Enjoy playing with more friends! 🎮**
